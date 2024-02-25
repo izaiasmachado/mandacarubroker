8ee26b04a6b7a8f88cb52d78192631319e9cfa87
JonasFortes12 <jonascforte@hotmail.com>
2024-02-25 16:00:57 -0300

    test: add tests for the get user by id route

--- src/test/java/com/mandacarubroker/controller/UserControllerTest.java
+++ src/test/java/com/mandacarubroker/controller/UserControllerTest.java
@@ -3,8 +3,11 @@ package com.mandacarubroker.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 
+import com.mandacarubroker.domain.user.RequestUserDTO;
 import com.mandacarubroker.domain.user.User;
 import com.mandacarubroker.service.UserService;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -14,10 +17,12 @@ import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.ResultMatcher;
 
+import java.time.LocalDate;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
@@ -33,6 +38,22 @@ class UserControllerTest {
 
     @Autowired
     private UserService service;
+
+    private final String urlRequestInvalidUser = "/users/dummy-user-id";
+    private User user;
+    private String userId;
+    private String urlRequestUserById;
+
+    @BeforeEach
+    void setUp() {
+        user = service.getAllUsers().get(0);
+        userId = user.getEmail();
+        urlRequestUserById = "/users/" + userId;
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 
 
     @Test
@@ -53,6 +74,30 @@ class UserControllerTest {
         assertEquals(service.getAllUsers().size(), users.size());
     }
 
+    @Test
+    void itShouldBeAbleToGetUserById() throws Exception {
+        String userJsonString = objectMapper.writeValueAsString(user);
+
+        RequestBuilder requestBuilder = get(urlRequestUserById);
+        ResultMatcher matchResponse = content().json(userJsonString);
+
+        mockMvc.perform(requestBuilder).andExpectAll(matchResponse);
+    }
+
+    @Test
+    void itShouldReturnOkStatusWhenGetUserById() throws Exception {
+        RequestBuilder requestBuilder = get(urlRequestUserById);
+        ResultMatcher matchStatus = status().isOk();
+
+        mockMvc.perform(requestBuilder).andExpect(matchStatus);
+    }
+
+    @Test
+    void itShouldReturnNotFoundWhenGetUserDoesNotExists() throws Exception {
+        RequestBuilder requestBuilder = get(urlRequestInvalidUser);
+        ResultMatcher matchStatus = status().isNotFound();
+        mockMvc.perform(requestBuilder).andExpect(matchStatus);
+    }
 
 
 }
