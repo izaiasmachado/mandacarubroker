49a2a5a721ef3c6850a0ac0079246fb1d397afc9
JonasFortes12 <jonascforte@hotmail.com>
2024-02-25 15:48:43 -0300

    test: add sql script to users table in the test database

--- src/test/resources/data.sql
+++ src/test/resources/data.sql
@@ -15,3 +15,24 @@ VALUES
     ('b2d13c5a-3df0-4673-b3e6-49244f395af9', 'AMZN4', 'Amazon.com Inc.', 400.00),
     ('b2d13c5a-3df0-4673-b3e6-49244f395a99', 'META5', 'Facebook Inc.', 500.00)
     ON CONFLICT (id) DO NOTHING;
+
+
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+                       email VARCHAR PRIMARY KEY,
+                       username VARCHAR NOT NULL,
+                       password VARCHAR NOT NULL,
+                       firstName VARCHAR NOT NULL,
+                       lastName VARCHAR,
+                       birthDate DATE NOT NULL,
+                       balance DECIMAL
+);
+
+INSERT INTO users (email, username, password, firstName, lastName, birthDate, balance)
+VALUES
+    ('joaopereira@hotmail.com', 'JoaoP', 'qwerty123', 'Joao', 'Pereira', '1988-07-05', 150.25),
+    ('maria.rodrigues@yahoo.com', 'MariaRod', 'pass456', 'Maria', 'Rodrigues', '1992-12-15', 75.60),
+    ('carlos.almeida@gmail.com', 'CarlosA', 'pass789', 'Carlos', 'Almeida', '1980-05-02', 500.00),
+    ('ana.silva@gmail.com', 'AnaSilva', 'xyz789', 'Ana', 'Silva', '1995-03-21', 300.50),
+    ('patricia.santos@hotmail.com', 'PatriciaS', 'pass123', 'Patricia', 'Santos', '1998-08-30', 250.75);
