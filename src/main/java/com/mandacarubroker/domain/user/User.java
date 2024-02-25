fd7f22b3895cf48123a29f2c26f82041f0d8de6c
JonasFortes12 <jonascforte@hotmail.com>
2024-02-25 18:19:05 -0300

    fix: add id attribute for users
    The lack of this attribute causes an error in the user update method, making it possible to send a request to update the email (old primary key)

--- src/main/java/com/mandacarubroker/domain/user/User.java
+++ src/main/java/com/mandacarubroker/domain/user/User.java
@@ -1,10 +1,13 @@
 package com.mandacarubroker.domain.user;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
@@ -14,9 +17,12 @@ import java.time.LocalDate;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(of = "id")
 public class User {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
     private String email;
     private String username;
     private String password;
