fe491d76d13ba71bc46c6bc83291aa51f6abb221
JonasFortes12 <jonascforte@hotmail.com>
2024-02-29 14:42:56 -0300

    feat: add jakarta bean validations in user DTO

--- src/main/java/com/mandacarubroker/domain/user/RequestUserDTO.java
+++ src/main/java/com/mandacarubroker/domain/user/RequestUserDTO.java
@@ -1,14 +1,29 @@
 package com.mandacarubroker.domain.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
+
 import java.time.LocalDate;
 
 public record RequestUserDTO(
+        @Email(message = "The email format is invalid")
         String email,
+        @NotBlank(message = "Username cannot be blank")
         String username,
+        @Size(min = MINIMUMPASSWORDLENGTH, message = "Password must be at least 8 characters long")
         String password,
+        @NotBlank(message = "First name cannot be blank")
         String firstName,
+        @NotBlank(message = "Last name cannot be blank")
         String lastName,
         LocalDate birthDate,
+        @NotNull(message = "Balance cannot be null")
+        @PositiveOrZero(message = "Balance cannot be negative")
         double balance
-) { }
+) {
+    private static final int MINIMUMPASSWORDLENGTH = 8;
+}
 
