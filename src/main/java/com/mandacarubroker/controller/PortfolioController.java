03cc2828941891b25fafa178084545599e41ceec
Izaias Machado <izaiasmachado.dev@gmail.com>
2024-03-05 15:59:07 -0300

    docs: portfolio controller routes with swagger

--- src/main/java/com/mandacarubroker/controller/PortfolioController.java
+++ src/main/java/com/mandacarubroker/controller/PortfolioController.java
@@ -2,6 +2,10 @@ package com.mandacarubroker.controller;
 
 import com.mandacarubroker.domain.position.ResponseStockOwnershipDTO;
 import com.mandacarubroker.service.PortfolioService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +16,7 @@ import org.springframework.web.server.ResponseStatusException;
 import java.util.List;
 import java.util.Optional;
 
+@Tag(name = "Portfólio de Ativos", description = "Operações relacionadas com portfólios de ativos")
 @RestController
 @RequestMapping("/portfolio")
 public class PortfolioController {
@@ -21,11 +26,20 @@ public class PortfolioController {
         this.portfolioService = receivedPortfolioService;
     }
 
+    @Operation(summary = "Listagem do portfólio de ativos do usuário", description = "Listagem do portfólio de ativos do usuário autenticado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Portfólio encontrado")
+    })
     @GetMapping
     public List<ResponseStockOwnershipDTO> getAuthenticatedUserStockPortfolio() {
         return portfolioService.getAuthenticatedUserStockPortfolio();
     }
 
+    @Operation(summary = "Detalhes sobre a posse de ações de uma empresa", description = "Mostra detalhes sobre a posse de ações de uma empresa por um usuário autenticado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Ação encontrada"),
+            @ApiResponse(responseCode = "404", description = "Ação não encontrada")
+    })
     @GetMapping("/stock/{stockId}")
     public ResponseStockOwnershipDTO getStockPositionById(@PathVariable final String stockId) {
         Optional<ResponseStockOwnershipDTO> stockPosition = portfolioService.getAuthenticatedUserStockOwnershipByStockId(stockId);
